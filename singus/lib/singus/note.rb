df1aa590706bda7d68c7c2fabb574fbcbefd7aab
Ville Peurala <ville.peurala@gmail.com>
2011-05-16 19:12:23 +0300

    Note parsing.

--- singus/lib/singus/note.rb
+++ singus/lib/singus/note.rb
@@ -1,10 +1,26 @@
 module Singus
   class Note
-    attr_accessor :pitch, :duration, :sound, :velocity
-  end
-end
+    attr_accessor :note_name, :pitch, :duration, :sound, :velocity
 
-class String
-  attr_accessor :pitch, :duration, :sound, :velocity
+    def self.parse(dsl)
+      n = Note.new
+      d = dsl.split("-")
+      n.note_name = d[0]
+      n.velocity = d[1].to_i
+      n.sound = d[2]
+      n.duration = parse_duration(d[3]) || 0.25
+      n
+    end
+
+    def self.parse_duration(input)
+      return nil if input.nil? || input.empty?
+      fractions = input.split('/').map{|s| s.strip.to_f}
+      return fractions[0] / fractions[1]
+    end
+
+    def ==(other)
+      @note_name == other.note_name && @velocity == other.velocity && @sound == other.sound && @duration == other.duration
+    end
+  end
 end
 
--- singus/spec/note_spec.rb
+++ singus/spec/note_spec.rb
@@ -1,18 +1,62 @@
 require 'spec_helper.rb'
 
 describe Note do
-  it "always should have velocity"
-  it "always should have note name"
-  it "always should have duration"
-  it "always should have sound"
+  Factory.define :note do |n|
+    n.velocity 122
+    n.note_name "Db5"
+    n.duration 1/4.0
+    n.sound "Horn"
+  end
+
+  def note 
+    Factory.build :note
+  end
+	
+  it "always should have velocity" do
+    note.velocity.should_not be_nil
+  end
+
+  it "always should have note name" do
+    note.note_name.should_not be_nil
+  end
+
+  it "always should have duration" do
+    note.duration.should_not be_nil
+  end
+  
+  it "always should have sound" do
+    note.sound.should_not be_nil
+  end
+
+  it "should be parsable from string with only note name" do
+    note = Note.parse "D4"
+    note.note_name.should == "D4"
+  end
+	
+  it "should be parsable from string with note name and velocity" do
+    note = Note.parse "C4-100"
+    note.velocity.should == 100
+  end
+
+  it "should be parsable from string with note name, velocity and sound" do
+    note = Note.parse "C4-100-Piano"
+    note.sound.should == "Piano"
+  end
+
+  it "should be parsable from string with note name, velocity, sound and duration" do
+    note = Note.parse "C4-100-Piano-1/8"
+    note.duration.should == 1/8.0
+  end
+	
+  it "should be parsable from string with any combination of the above in any order" do
+    parsed_note = Note.parse "Db5-122-Horn-1/4"
+    parsed_note.should == note
+  end
+
+  it "should not require strict formatting in obvious cases"
+
   it "should have the previous sound used if no explicit sound given"
   it "should have the previous duration used if no explicit duration given"
   it "should have the previous velocity used if no explicit velocity given"
   it "should have the previous note name if no explicit note name given"
-  it "should be parsable from string with only note name"
-  it "should be parsable from string with note name and velocity"
-  it "should be parsable from string with note name, velocity and sound"
-  it "should be parsable from string with note name, velocity, sound and duration"
-  it "should be parsable from string with any combination of the above in any order"
-  it "should not require strict formatting in obvious cases"
 end
